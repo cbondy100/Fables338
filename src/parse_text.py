c786390d6b514b1960f43111b1bdec46a372ac2e
Callum Bondy <callum.bondy@gmail.com>
2022-10-20 10:15:17 -0500

    textblob practice

--- src/parse_text.py
+++ src/parse_text.py
@@ -1,2 +1,17 @@
 #Parsing text API practice
 #test git push
+
+#importing TextBlob
+from textblob import TextBlob
+
+#TestBlob practice
+test = TextBlob("A buck, pursued by hunters, concealed himself among the branches of a vine.")
+
+print(test.tags)
+
+kept_words = []
+for tuple in test.tags:
+    if tuple[1] == 'NN':
+        kept_words.append(tuple[0])
+
+print(kept_words)
