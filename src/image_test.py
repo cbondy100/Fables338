1c79c34e30a26f2f5cf23e768cda61d1edbaca8e
Callum Bondy <callum.bondy@gmail.com>
2022-10-24 16:29:05 -0500

    simple changes

--- src/image_test.py
+++ src/image_test.py
@@ -1,4 +1,4 @@
 from dalle2 import Dalle2
 dalle = Dalle2("sess-YMsgY2QM2aP3TSK6P2jFwNpsSAhNOTMvsxvNI3d2")
-file_paths = dalle.generate_and_download("portal to another dimension, digital art")
+file_paths = dalle.generate_and_download("futuristic cat with a sword in a boat, digital art")
 print(file_paths)
