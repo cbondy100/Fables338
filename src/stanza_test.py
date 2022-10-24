37b715cf03fcacca7e0e9429228552e035d900c2
Callum Bondy <callum.bondy@gmail.com>
2022-10-24 18:32:44 -0500

    text parsing

--- src/stanza_test.py
+++ src/stanza_test.py
@@ -5,8 +5,20 @@ import stanza
 nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
 doc = nlp("A buck, pursued by hunters, concealed himself among the branches of a vine")
 
+pos_exlude = ['ADV', 'DET', 'INTJ', 'PART', 'PUNCT', 'SYM', 'X']
+
 #POS tags
 #print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
 
 #depparse
-print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
+#print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
+
+#constituency processor
+#print(doc.sentences[0].constituency)
+
+final_string_list = []
+for word in doc.sentences[0].words:
+    if word.upos not in pos_exlude:
+        final_string_list.append(word.lemma)
+
+print(' '.join(final_string_list))
