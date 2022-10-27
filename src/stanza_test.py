10fc5bd9fd730a62661512ce6940d20a52bda198
Callum Bondy <callum.bondy@gmail.com>
2022-10-27 11:36:51 -0500

    nothing new

--- src/stanza_test.py
+++ src/stanza_test.py
@@ -10,7 +10,7 @@ Next Steps:
 
 #stanza.download('en')
 nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
-doc = nlp("A buck, pursued by hunters, concealed himself among the branches of a vine.")
+doc = nlp("One of the hunters, attracted by the rustling, turned round and guessing that their prey was there, shot into the bush and killed him.")
 
 pos_exlude = ['ADV', 'DET', 'INTJ', 'PART', 'PUNCT', 'SYM', 'X']
 
@@ -18,14 +18,16 @@ pos_exlude = ['ADV', 'DET', 'INTJ', 'PART', 'PUNCT', 'SYM', 'X']
 #print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
 
 #depparse
-#print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
+print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
 
 #constituency processor
 #print(doc.sentences[0].constituency)
 
+"""
 final_string_list = []
 for word in doc.sentences[0].words:
     if word.upos not in pos_exlude:
         final_string_list.append(word.lemma)
 
 print(' '.join(final_string_list))
+"""
