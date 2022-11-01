a5aa979265558c645b29ed8937c9921c4cbfadb3
Callum Bondy <callum.bondy@gmail.com>
2022-10-31 20:24:10 -0500

    very simple demo done

--- src/pipeline_demo.py
+++ src/pipeline_demo.py
@@ -1,14 +1,39 @@
 import stanza
 from dalle2 import Dalle2
 
+pos_exlude = ['ADV', 'DET', 'INTJ', 'PART', 'PUNCT', 'SYM', 'X']
+
 def imageGeneration(text):
     dalle = Dalle2("sess-q3JpYmKLzWgsiXdbQOAYZpKT5p7jtALHN8zy3DEI")
+    
     file_paths = dalle.generate_and_download(text)
+    return file_paths
 
 def textProcessing(text):
+    #very basic processing
     nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
     doc = nlp(text)
-    return 
+
+    final_string_list = []
+    for word in doc.sentences[0].words:
+        if word.upos not in pos_exlude:
+            final_string_list.append(word.text)
+
+    return " ".join(final_string_list)
+
+def print_file_paths(file_paths):
+    for path in file_paths:
+        print(path + "\n")
 
 if __name__ == '__main__':
+    print("Please input a one sentence scene of a story")
     input_string = str(input())
+    print("What style would you link your art in? (comic, digital art, drawing)")
+    input_style = str(input())
+
+    processed_text = textProcessing(input_string)
+    processed_text = processed_text + " " + input_style + " style"
+    print(processed_text)
+    file_paths = imageGeneration(processed_text)
+    print("Your generated images are at these paths:\n")
+    print_file_paths(file_paths)
