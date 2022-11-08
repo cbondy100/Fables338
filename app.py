4c1ff2b77176d865ed7ffdfbe5a8614168357224
Callum Bondy <callum.bondy@gmail.com>
2022-11-08 12:10:28 -0600

    new html file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from pipeline_demo import imageGenerator
+from flask import Flask, request, render_template
+#from pipeline_demo import imageGenerator
 
 app = Flask(__name__, template_folder="templateFiles", static_folder="staticFiles")
 
@@ -9,9 +9,11 @@ def index():
 
 @app.route("/result")
 def result():
+    result = request.form['storyinput']
     input_string = ''
     input_style = ''
-    imageGenerator.main(input_string, input_style)
+    #imageGenerator.main(input_string, input_style)
+    return render_template('result.html', result = result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
