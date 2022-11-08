2f5c93e6d07bf7b20e7e6391e7a21afb710821be
patrickdwyer33 <patrickdwyer2023@u.northwestern.edu>
2022-11-08 11:50:36 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from pipeline_demo import imageGenerator
 
 app = Flask(__name__, template_folder="templateFiles", static_folder="staticFiles")
 
@@ -8,7 +9,9 @@ def index():
 
 @app.route("/result")
 def result():
-    
+    input_string = ''
+    input_style = ''
+    imageGenerator.main(input_string, input_style)
 
 if __name__ == '__main__':
     app.run()
