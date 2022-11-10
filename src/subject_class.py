5582ae7051ab29bdd5999c9fda2b2b82464c9bed
Callum Bondy <callum.bondy@gmail.com>
2022-11-10 11:17:20 -0600

    subject_class

--- src/subject_class.py
+++ src/subject_class.py
@@ -2,6 +2,9 @@
 import stanza
 import os
 
+pos_exclude = ['ADV', 'DET', 'INTJ', 'PART', 'PUNCT', 'SYM', 'X']
+
+
 class Subject:
     # initialize noun pairings, attributes
     def __init__(self, nouns, attributes):
@@ -11,12 +14,20 @@ class Subject:
     def getSubject():
         pass
 
+def getAttributes(doc):
+    for word in doc.sentences[0].words:
+        pass
 
 def textProcessing(text):
     nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
     doc = nlp(text)
-    print(doc.sentences)
+    for word in doc.sentences[0].words:
+        if word.upos in set(pos_exclude):
+            doc.sentences[0].words.remove(word)
+        if word.upos == "NOUN":
+            subject = Subject(word.lemma, )
+    new_doc = doc.sentences[0].words
 
 if __name__ == "__main__":
     input_string = "There once was a green ogre named Jefferey"
-    processed_text = text_processing(input_string)
+    processed_text = textProcessing(input_string)
