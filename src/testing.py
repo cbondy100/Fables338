b5f2abb2187aa649620ce1e46e7d5f93cab520e7
Callum Bondy <callum.bondy@gmail.com>
2022-11-13 22:28:15 -0600

    attribute carry work

--- src/testing.py
+++ src/testing.py
@@ -4,7 +4,7 @@ import os
 
 pos_exclude = ['ADV', 'DET', 'INTJ', 'PART', 'PUNCT', 'SYM', 'X']
 
-noun_list = []
+#noun_list = []
 
 def textProcessing(text):
     nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
@@ -34,15 +34,33 @@ def addToDict(doc):
             #print(attr_list)
             mydict['attr'] = attr_list
             #print(mydict)
-    noun_list.append(mydict)
+    #noun_list.append(mydict)
     #it is currently replacing the old noun when it sees a new one,
     # have to only add attributes whose "head" is equal to the noun
-    
 
+def dictTest(doc, noun_list):
+    attr_list = []
+    mydict = {}
+    for word in doc:
+        #check if word is already in dictionary list, then update data if so
+        if word.upos == "NOUN":
+            mydict['noun'] = word.text
+            attr_list = []
+            for subword in doc:
+                if subword.head == word.id:
+                    attr_list.append(subword.text)
+                    mydict['attr'] = attr_list
+            print(mydict)
+            noun_list.append(mydict)
+            
+    
 if __name__ == "__main__":
-    input_string = "A Fox one day spied a beautiful bunch of ripe grapes hanging from a vine trained along the branches of a tree. The grapes seemed ready to burst with juice, and the Fox's mouth watered as he gazed longingly at them."
+    input_string = "A Fox one day spied a beautiful bunch of ripe grapes hanging from a vine trained along the branches of a tree."
     proc_doc = textProcessing(input_string)
+
+    noun_list = []
     print(proc_doc)
     for doc in proc_doc:
-        addToDict(doc)
+        #addToDict(doc)
+        dictTest(doc, noun_list)
     print(noun_list)
