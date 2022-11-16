bae0ebba68100472a480fb0c14451d2c00ca154e
Callum Bondy <callum.bondy@gmail.com>
2022-11-15 20:29:35 -0600

    i did a lot lol

--- app.py
+++ app.py
@@ -12,10 +12,28 @@ def index():
 def result():
     if request.method == 'POST':
         result = request.form
+        input_title = result['titleinput']
         input_string = result['storyinput']
-        input_style = 'comic'
-        file_to_display = imageGenerator.main(input_string, input_style)
-        return render_template('result.html', result = file_to_display)
+        input_style = result['style_chosen'] + " style"
+
+        sen_list = input_string.split(". ")
+        print(sen_list)
+
+        images_list = imageGenerator.main(input_string, input_style)
+        
+        
+        scene_data = []
+        for index in range(len(sen_list)):
+            scene_data.append([images_list[index], sen_list[index]])
+
+        all_data={
+            "title":input_title,
+            "style": input_style,
+            "scene_list":scene_data
+            }
+        
+        #file_to_display = imageGenerator.main(input_string, input_style)
+        return render_template('result.html', data=all_data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- src/pipeline_demo.py
+++ src/pipeline_demo.py
@@ -9,20 +9,25 @@ class imageGenerator:
 #sess-Y5PgcdYxRMvqfK8YMUvGVpZdGNTrNNVDi3UJfau5
 #sess-q3JpYmKLzWgsiXdbQOAYZpKT5p7jtALHN8zy3DEI
     @classmethod
-    def imageGeneration(cls, text):
+    def imageGeneration(cls, text, style):
         #dalle = Dalle2("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")
         #images = dalle.generate(text)
-        text = text[0]
+        #text = text[0]
         url = "https://api.openai.com/v1/images/generations"
-        headers = {"Content-Type": "application/json", "Authorization": "Bearer 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"}
-        data = {"prompt": text, "n": 4, "size": "1024x1024"}
-        images = requests.post(url, json = data, headers = headers)
-        print(images)
-        returned = images.json()
-        print(returned)
-        output_url = returned['data'][0]['url']
+        headers = {"Content-Type": "application/json", "Authorization": "Bearer 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"}
+        
+        url_list = []
+
+        for scene in text:
+            data = {"prompt": scene + style, "n": 4, "size": "1024x1024"}
+            images = requests.post(url, json = data, headers = headers)
+            print(images)
+            returned = images.json()
+            print(returned)
+            output_url = returned['data'][0]['url']
+            url_list.append(output_url)
         #file_paths = dalle.download(images)
-        return output_url
+        return url_list
 
     @classmethod
     def textProcessing(cls, text):
@@ -30,7 +35,7 @@ class imageGenerator:
         nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
         doc = nlp(text)
 
-        final_strings = []
+        final_proc_strings = []
 
         for sentence in doc.sentences:
             cur_string = []
@@ -38,9 +43,9 @@ class imageGenerator:
                 if word.upos not in set(cls.pos_exclude):
                     cur_string.append(word.text)
             cur_string = ' '.join(cur_string)
-            final_strings.append(cur_string)
+            final_proc_strings.append(cur_string)
 
-        return final_strings
+        return final_proc_strings
 
     @classmethod
     def print_file_paths(cls, file_paths):
@@ -53,7 +58,8 @@ class imageGenerator:
             if file.endswith('.webp'):
                 os.remove(file)
         processed_text = cls.textProcessing(input_string)
-        return cls.imageGeneration(processed_text)
+        #return cls.imageGeneration(processed_text)
+        return cls.imageGeneration(processed_text, input_style)
 
 if __name__ == '__main__':
     print(imageGenerator.main('car', 'comic'))
