b1138c5c6aaabf67a68bb798e4dd808c821bd307
Callum Bondy <callum.bondy@gmail.com>
2022-11-29 15:43:58 -0600

    nothing new

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ def result():
         input_string = result['storyinput']
         input_title = result['titleinput']
         input_style = result['style_chosen']
-        story_obj = Story(input_string, input_title, input_style)
+        #story_obj = Story(input_string, input_title, input_style)
 
         #use for html / css edits
-        #raw_sentences = input_string.split(". ")
-        #scene_data = []
-        #for s in raw_sentences:
-        #    scene_data.append(["https://readtheforum.org/wp-content/uploads/2018/10/Screen-Shot-2018-10-01-at-7.17.51-PM.png", s])
-
+        raw_sentences = input_string.split(". ")
         scene_data = []
-        for scene in story_obj.scenes:
-            scene_data.append([scene.image, scene.raw_sentence])
+        for s in raw_sentences:
+            scene_data.append(["https://readtheforum.org/wp-content/uploads/2018/10/Screen-Shot-2018-10-01-at-7.17.51-PM.png", s])
+
+        #scene_data = []
+        #for scene in story_obj.scenes:
+        #    scene_data.append([scene.image, scene.raw_sentence])
 
         all_data={
             "title":input_title,
